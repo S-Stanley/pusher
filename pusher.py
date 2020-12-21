8cfa8d87f7966deeb13eed21b6e863071e21f64a
Stanley SERBIN <stanleyserbin@gmail.com>
2020-12-21 03:39:45 +0100

    README

--- pusher.py
+++ pusher.py
@@ -6,7 +6,7 @@ if len(sys.argv) == 1:
 	print('Erreur! Vous devez indiquer un message')
 	sys.exit()
 if len(sys.argv) > 2:
-	print('Erreur, vous avez indiquer trop d\'argument, essayez avec des appostrophes "')
+	print('Erreur, vous avez indiquer trop d\'argument, essayez avec des apostrophes "')
 	sys.exit()
 
 message = sys.argv[1]
@@ -16,8 +16,6 @@ if '.git' not in files:
 if 'origin' not in os.popen('git remote').read().split('\n'):
 	origin = input('Add git origin url: ')
 	os.popen(f'git remote add origin {origin}')
-else:
-	origin = os.popen('git remote get-url origin').read().replace('\n', '')
 os.popen('git add .')
 os.popen(f'git commit -m "{message}"')
 os.popen('git pull origin master --allow-unrelated-histories')
