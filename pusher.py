1452bddadb787be0fec369b8103ca69e47f74891
Stanley SERBIN <stanleyserbin@gmail.com>
2020-12-21 04:04:16 +0100

    UPDATE readme

--- pusher.py
+++ pusher.py
@@ -11,8 +11,8 @@ if len(sys.argv) > 2:
 message = sys.argv[1]
 all_branch = os.popen('git branch').read().split('\n')
 for br in all_branch:
-	if '*' in all_branch:
-		branch = br
+	if '*' in br:
+		branch = br.replace('*', '').replace(' ', '')
 		break
 files = os.popen('ls -a').read().split('\n')
 if '.git' not in files:
@@ -20,6 +20,6 @@ if '.git' not in files:
 if 'origin' not in os.popen('git remote').read().split('\n'):
 	origin = input('Add git origin url: ')
 	os.popen(f'git remote add origin {origin}').read()
-os.popen(f'git add . && git commit -m {message} && git pull origin {branch}').read()
+os.popen(f'git add . && git commit -m "{message}" && git pull origin {branch}').read()
 os.popen(f'git push origin {branch}').read()
 print('Done !')
