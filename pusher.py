3e79f6a497c7ddf71001a5cce42c8866eae5c988
Stanley SERBIN <stanleyserbin@gmail.com>
2020-12-21 20:27:16 +0100

    PUSH made by pusher https://github.com/S-Stanley/pusher.git

--- pusher.py
+++ pusher.py
@@ -9,20 +9,6 @@ if len(sys.argv) > 2:
 	print('Erreur, vous avez indiquer trop d\'argument, essayez avec des apostrophes "')
 	sys.exit()
 
-# if '.git' not in files:
-# 	os.popen('git init').read()
-# all_branch = os.popen('git branch').read().split('\n')
-# for br in all_branch:
-# 	if '*' in br:
-# 		branch = br.replace('*', '').replace(' ', '')
-# 		break
-# files = os.popen('ls -a').read().split('\n')
-# if 'origin' not in os.popen('git remote').read().split('\n'):
-# 	origin = input('Add git origin url: ')
-# 	os.popen(f'git remote add origin {origin}').read()
-# os.popen(f'git add . && git commit -m "{message}" && git pull origin {branch}').read()
-# os.popen(f'git push origin {branch}').read()
-
 push = Commit(message)
 push.check_repo()
 push.get_remote()
